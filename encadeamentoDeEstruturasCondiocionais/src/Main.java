77f8ee1bf7415c95d3c68ec4732e65cc49921b73
Daniel <daniel.ilario.oliveira@gmail.com>
2023-03-01 17:39:08 -0300

    feat: cria o programa encadeamentoDeEstruturasCondicionais

---
+++ encadeamentoDeEstruturasCondiocionais/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int hora;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Que horas são?");
+		hora = sc.nextInt();
+		if(hora < 12) {
+			System.out.println("Bom dia");
+			}
+		else{
+			if(hora >= 12 && hora < 19) {
+				System.out.println("Boa tarde");
+			}
+			else {
+				System.out.println("Boa noite");
+			}
+		}
+		
+		sc.close();
+	}
+
+}
